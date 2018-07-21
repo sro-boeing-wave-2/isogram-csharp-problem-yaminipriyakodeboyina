08254a78a9473b3ca17406376178d882881b3a3b
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-07-21 15:52:46 +0530

    last commit

---
+++ Isogram.Tests/isogramtest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xunit;
+using Isogram;
+namespace Isogramtest
+{
+    public class isogramtest
+    {
+        [Fact]
+        public void Sentence_empty()
+        {
+            var result = isogram.Isisogram("");
+            Assert.True(result);
+        }
+        [Fact]
+        public void Lowercase_isogram()
+        {
+            var result = isogram.Isisogram("isogram");
+            Assert.True(result);
+        }
+        [Fact]
+        public void Lowercase_eleven()
+        {
+            var result = isogram.Isisogram("eleven");
+            Assert.False(result);
+        }
+        [Fact]
+        public void Lowercase_subdermatoglyphic()
+        {
+            var result = isogram.Isisogram("subdermatoglyphic");
+            Assert.True(result);
+        }
+        [Fact]
+        public void Lower_uppercase_Alphabet()
+        {
+            var result = isogram.Isisogram("Alphabet");
+            Assert.True(result);
+        }
+        [Fact]
+        public void Lower_uppercase_hiphen()
+        {
+            var result = isogram.Isisogram("thumbscrew-japingly");
+            Assert.True(result);
+        }
+        [Fact]
+        public void Lower_duphyphent()
+        {
+            var result = isogram.Isisogram("six-year-old");
+            Assert.True(result);
+        }
+        [Fact]
+        public void Lower_dupspaces()
+        {
+            var result = isogram.Isisogram("six year old");
+            Assert.True(result);
+        }
+    }
+} 
---
+++ Isogram/isogram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Isogram
+{
+    public class isogram
+    {
+        public static bool Isisogram(string input)
+        {
+            for (int i = 0; i<input.Length - 1; i++)
+            {
+                if (char.IsLetter(input[i]))
+                {
+                    if (input.IndexOf(input[i], i + 1) != -1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
